caa13a5e73630cf1f91561cf3527b8b434ce7e80
Erik Garrison <erik.garrison@gmail.com>
2023-08-04 19:20:25 +0200

    implement parallel parsing of input

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use rust_htslib::faidx::Reader;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::io::Write;
+use std::sync::{Arc, Mutex};
+use rayon::prelude::*;
 
 /// Search for a specific sequence in the human pangenome
 #[derive(Parser, Debug)]
@@ -22,6 +25,10 @@ struct Args {
     /// Maximum number of mismatches allowed (Hamming distance)
     #[arg(short, long, default_value_t = 6)]
     distance: usize,
+
+    /// Number of threads for parallel execution
+    #[arg(short = 't', long, default_value = "0")]
+    parallelism: usize,
 }
 
 fn load_fai(path: &str) -> HashMap<String, usize> {
@@ -46,37 +53,45 @@ fn hamming_distance(s1: &str, s2: &str) -> usize {
     s1.chars().zip(s2.chars()).filter(|&(c1, c2)| c1 != c2).count()
 }
 
-fn search_sequence(fasta: &str, target: &str, prefix: &str, max_mismatches: usize)  {
+fn search_sequence(fasta: &str, target: &str, prefix: &str, max_mismatches: usize) {
     let reader = Reader::from_path(fasta).unwrap();
     let n_seqs = reader.n_seqs();
     let seq_lengths = load_fai(fasta);
+
+    let stdout_lock = Arc::new(Mutex::new(std::io::stdout()));
+
     // print a header line in tsv
     println!("seq_name\tstart\tend\tsequence\tmismatches");
-    for i in 0..n_seqs {
-        let seq_name = reader.seq_name(i as i32).unwrap();
 
-        // Skip sequences that don't match the prefix
+    (0..n_seqs).into_par_iter().for_each(|i| {
+        let reader = Reader::from_path(fasta).unwrap(); // Re-create the reader for thread safety
+        let seq_name = reader.seq_name(i as i32).unwrap();
         if !prefix.is_empty() && !seq_name.starts_with(prefix) {
-            continue;
+            return;
         }
         let seq_length = seq_lengths.get(&seq_name).unwrap();
         let sequence_str = reader.fetch_seq_string(&seq_name, 0, *seq_length).unwrap();
-        //println!("Searching sequence {} of length {}", seq_name, sequence_str.len());
 
         for (idx, window) in sequence_str.as_bytes().windows(target.len()).enumerate() {
             let window_str = std::str::from_utf8(window).unwrap();
             let distance = hamming_distance(window_str, target);
             if distance <= max_mismatches {
-                //println!("Match found in sequence {}: {}", seq_name, window_str);
-                println!("{}\t{}\t{}\t{}\t{}", seq_name, idx, idx+window.len(), window_str, distance);
+                let mut stdout = stdout_lock.lock().unwrap();
+                writeln!(stdout, "{}\t{}\t{}\t{}\t{}", seq_name, idx, idx + window.len(), window_str, distance).unwrap();
             }
         }
-    }
+    });
 }
 
 fn main() {
     let args = Args::parse();
-
+    // If parallelism is set to 0, use the default (number of available CPU cores)
+    if args.parallelism > 0 {
+        rayon::ThreadPoolBuilder::new()
+            .num_threads(args.parallelism)
+            .build_global()
+            .unwrap();
+    }
     search_sequence(&args.fasta, &args.sequence, &args.prefix, args.distance);
 
 }
